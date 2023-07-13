d0ac57e26d98e3d7591068637c2edd3641db5751
Shimane Akiho <s230011@std.it-college.ac.jp>
2023-07-13 07:59:21 +0000

    chapter03add

---
+++ chapter03/Q3_2_1.py
@@ -0,0 +1,5 @@
+a = 10
+if a > 10:
+    print(a / 2)
+else:
+    print(a + 1)
---
+++ chapter03/Q3_2_2.py
@@ -0,0 +1,8 @@
+a = 20
+if a >= 100:
+    print(a * 2)
+elif a > 50:
+    print(a / 2)
+else:
+    print(a + 2)
+print(a)
